ce0d9d565d47ffe75829f6fe5eb508a93dc21877
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-03 10:54:00 -0500

    basic

---
+++ InterviewPrepAPI/src/main/java/com/myinterviewprep/InterviewPrepAPI/InterviewPrepApiApplication.java
@@ -0,0 +1,13 @@
+package com.myinterviewprep.InterviewPrepAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPrepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPrepApiApplication.class, args);
+	}
+
+}
---
+++ InterviewPrepAPI/src/test/java/com/myinterviewprep/InterviewPrepAPI/InterviewPrepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myinterviewprep.InterviewPrepAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
